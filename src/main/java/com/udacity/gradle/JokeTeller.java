43de7bf4f02d55fc0f435e49c051b0509a316b53
Sasi <sasikumar@giffgaff.co.uk>
2016-04-18 00:11:06 +0100

    Joke teller
    Build a joke teller demo - initially built on a gradle file and then moved to a java class calling from the gradle script. This is so exciting to know completely about the android environment.

---
+++ src/main/java/com/udacity/gradle/JokeTeller.java
@@ -0,0 +1,15 @@
+package com.udacity.gradle;
+
+
+public class JokeTeller {
+
+    public static void main(String[] args) {
+//        System.out.println("Java. Write once, debug everywere.");
+
+        System.out.println("What's a build master's favorite black metal band?");
+        System.out.println("Gradle of Filth!");
+
+        System.out.println("What's a second Joke?");
+        System.out.println("Not a joke man!");
+    }
+}
